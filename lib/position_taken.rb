80a9741d0c072352cd1f758bb68fbce525267ec1
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-11-04 19:53:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,7 @@ def position_taken? (board, index)
   #if (board[index] == "")
  if ((board[index] == " " ) || (board[index] == "") || (board[index] == nil))
    return false
- end
- else if ((board[index] == "X") || (board[index] == "O"))
+ elsif ((board[index] == "X") || (board[index] == "O"))
    return true
  end
 end
