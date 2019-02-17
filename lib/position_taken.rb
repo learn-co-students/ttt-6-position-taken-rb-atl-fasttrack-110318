575b9c1c6934051908939d1525ca8bc9a24692c8
Preston Elliott <preston.elliott24@gmail.com>
2019-02-17 17:52:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,21 @@
 # code your #position_taken? method here!
 
+# board is an array of strings
+# ["X", "", " ", "0", etc]
+# index is a number to check 
+# index values are 0 through 8
+
+
 def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
+# ANOTHER SOLUTION
+# def position_taken?(board, index)
+#   position = board[index]
+#   if position == "" || position == " " || position == nil
+#     return false 
+#   else
+#     return true 
+#   end
+# end
