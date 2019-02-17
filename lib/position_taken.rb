85d1f1e83df8aa04614ae0b1dbe534a57c08e5d0
Dylan Harris <dylanharris23@gmail.com>
2019-02-17 18:12:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
+#board is an arry of strings 
+# ["x", ""," ", "",]
+# index is a number to check 
+# it could be 0, 4, 7, 1
 # code your #position_taken? method here!
+def position_taken?(board, index)
+   if board[index] == "" || board[index] == " " || board[index] == nil
+     return false
+ else
+     return true
+  end
+ end
